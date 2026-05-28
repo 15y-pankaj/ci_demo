42b908c7309402b71f6e3f242c16f4b931968dca
15y-pankaj <15y.pankaj@gmail.com>
2026-05-28 12:02:43 +0530

    ci_demo

---
+++ main.py
@@ -0,0 +1,27 @@
+import matplotlib.pyplot as plt
+
+fig,ax = plt.subplots()
+
+fruits = ['Apple', 'Banana', 'Cherry', 'Orange']
+counts=[10, 15, 7, 12]
+bar_labels = ['red', 'yellow', 'pink', 'orange']
+bar_colors = ['tab:red', 'yellow', 'tab:pink', 'tab:orange']
+
+ax.bar(fruits, counts, color=bar_colors)
+
+ax.set_ylabel('fruit supply')
+ax.set_title('Fruit Supply by Kind and Type')
+ax.legend(title='Fruit Types')
+
+plt.savefig('fruit_supply.png',bbox_inches='tight')
+
+cat=['bored','happy','happy','happy','happy','bored']
+dog=['happy','happy','happy','happy','bored','bored']
+actions=['combing','drinking','feeding','napping','playing','walking']
+
+fig,ax = plt.subplots()
+ax.plot(actions,dog,label='dog')
+ax.plot(actions,cat,label='cat')
+ax.legend()
+
+plt.savefig('lines.png',bbox_inches='tight')
